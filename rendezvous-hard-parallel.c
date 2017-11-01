0b2e08da509489e318634678f80e6c20c1966940
Jhone Darts <jhonedarts@gmail.com>
2017-10-31 23:09:26 -0300

    Condicao de quebra do loop mais interno quando achar o rendezvous

--- rendezvous-hard-parallel.c
+++ rendezvous-hard-parallel.c
@@ -115,7 +115,7 @@ void main(int argc, char *argv[]) {
 								printf("Thread %d: RANDEZVOUS %lf %lf %lf %lf %lf %lf %lf %lf %lf %d\n", ID, x, y, z, xl0, yl0, zl0, X, gama, Ve, t);
 								fprintf(out, "RANDEZVOUS %lf %lf %lf %lf %lf %lf %lf %lf %lf %d\n", x, y, z, xl0, yl0, zl0, X, gama, Ve, t);
 								printf("R: %lf V:%lf\n", r,v);
-								//t = Tmax + 1;
+								t = Tmax + 1;
 							}
 						}
 						//printf(" ======== Saídas Finais ========\n");
